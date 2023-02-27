dd483cf3ef5a70172a108c9b1adc3a98c70ecdf1
Andrzej Broński <andrzej@casper.network>
2023-02-27 10:26:27 +0100

    Update announcement bar text - no specific time

--- config/announcement.config.js
+++ config/announcement.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     id: "announcement",
-    content: "Documentation is undergoing changes for our new <b>Developer Portal</b> launching in Q1. Please excuse any disruptions.",
+    content: "This documentation is changing to align with a new <b>Developer Portal</b> launching soon. Please excuse any disruptions.",
     backgroundColor: "#1c1e21",
     textColor: "#fff",
     isCloseable: false,
