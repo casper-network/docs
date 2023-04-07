60fb2a2882dd4788076c4306ab0b7db7e4a60438
Adam Stone <97986246+ACStoneCL@users.noreply.github.com>
2023-04-07 09:48:04 -0400

    Removing FAQ change that crept in

--- config/sidebar.config.js
+++ config/sidebar.config.js
@@ -300,6 +300,7 @@ module.exports = {
         "users/funding-from-exchanges",
         "users/testnet-faucet",
     ],
+    faq: ["faq/index", "faq/faq-general", "faq/faq-enterpise", "faq/faq-developer", "faq/faq-validator"],
     workflow: ["workflow/ledger-setup"],
     // BELOW ITEMS ARE COMMENTED OUT BECAUSE WE COULD NOT LOCATE PLACE FOR THEM IN THE NEW STRUCTURE:
     /*"dapp-dev-guide": [
@@ -311,8 +312,6 @@ module.exports = {
             collapsed: true,
             items: [
                 "dapp-dev-guide/building-dapps/dapp",
-                "dapp-dev-guide/building-dapps/setup-nctl",
-                "dapp-dev-guide/building-dapps/nctl-test",
             ],
         },
     ],*/
