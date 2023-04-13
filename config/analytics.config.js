afc3d13aa07fa1a14eb0303003257ec5be93f408
Andrzej Broński <andrzej@casper.network>
2023-04-13 21:01:56 +0200

    Simplify analytics config

--- config/analytics.config.js
+++ config/analytics.config.js
@@ -2,8 +2,6 @@ const tagTrackingId = process.env.GOOGLE_TAG_TRACKING_ID;
 const anonymizeIP = process.env.GOOGLE_TAG_ANONYMIZE_IP || true;
 
 module.exports = {
-    gtag: {
-        trackingID: tagTrackingId,
-        anonymizeIP: anonymizeIP,
-    },
+    trackingID: tagTrackingId,
+    anonymizeIP: anonymizeIP,
 };
